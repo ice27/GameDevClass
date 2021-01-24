761167b3e18f2977bfb42014a430dade49944a9c
Ice on Gibabyte <ice@icejohnson.com>
2021-01-23 20:54:33 -0700

    finished chapter

---
+++ Assets/Scripts/FSInput.cs
@@ -0,0 +1,40 @@
+﻿//Lisa Ice
+//lice@cnm.edu
+//Game Development, CIS 2250
+//UIA Chapter 2 Assignment
+//Created 1/23/21
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/ FPS Input")]
+
+public class FSInput : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private CharacterController _charController;
+    public float gravity = -9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+
+    }
+}

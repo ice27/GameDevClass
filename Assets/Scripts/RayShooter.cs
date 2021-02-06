50b19c489403a5803dd04fb988bc2c3fae43894c
Ice on Gibabyte <ice@icejohnson.com>
2021-02-06 12:00:23 -0700

    done with section 3 -- sphere indicator and cursor

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -6,11 +6,16 @@ public class RayShooter : MonoBehaviour
 {
 
     private Camera _camera;
+    public int SizeOfCursor = 12;
+    public float SizeOfSphereIndicator = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         _camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -33,8 +38,18 @@ public class RayShooter : MonoBehaviour
     {
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         sphere.transform.position = pos;
+        Vector3 scale = new Vector3(SizeOfSphereIndicator / 10.0f, SizeOfSphereIndicator / 10.0f, SizeOfSphereIndicator / 10.0f);
+        sphere.transform.localScale = scale;
         yield return new WaitForSeconds(1);
 
         Destroy(sphere);
+    }
+
+    private void OnGUI()
+    {
+        int size = SizeOfCursor;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
     }
 }

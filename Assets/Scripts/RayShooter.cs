c482e0a3aa7d7198e5ada13dd5e7f5e1eafe03f9
Ice on Gibabyte <ice@icejohnson.com>
2021-02-06 11:35:27 -0700

    added sphere indicator

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -23,8 +23,18 @@ public class RayShooter : MonoBehaviour
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit))
             {
-                Debug.Log("Hit " + hit.point);
+                //Debug.Log("Hit " + hit.point);
+                StartCoroutine(SphereIndicator(hit.point));
             }
         }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
     }
 }
